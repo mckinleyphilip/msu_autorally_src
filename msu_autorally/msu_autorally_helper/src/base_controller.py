98bc3e6fa5a33d906fbec292d2440b96c5c93ac4
Glen Simon <simongle@msu.edu>
2018-06-14 08:25:02 -0400

    Modified physics setting for faster real time factor. Beginning work on adding reverse capabilities and changing the base_controller to utilize the constant speed controller to control throttle.

--- msu_autorally/msu_autorally_helper/src/base_controller.py
+++ msu_autorally/msu_autorally_helper/src/base_controller.py
@@ -17,6 +17,7 @@ import collections
 
 from geometry_msgs.msg import Twist
 from autorally_msgs.msg import chassisCommand
+from autorally_msgs.msg import wheelSpeeds
 
 
 
@@ -26,14 +27,37 @@ class BaseController():
 		self.debugging = args.debug
 		self.name = node_name
 		self.throttle_mappings = {}
+		
+		self.most_recent_twist_cmd = 0
+		self.goal_speed = 0
+		self.goal_steering = 0
+		self.front_wheel_speed = 0
+		
+		self.steering = -5
+		self.throttle = -5
+		self.frontBrake = -5
+		
+		
+		# PID Controller variables
+		self.integral_error = 0
+		self.throttle_KP = 0.15
+		self.throttle_KD = 0.0
+		self.throttle_KI = 0.001
+		self.throttle_IMax = 0.2
+		
+		
+		self.load_throttle_calibration()
 		 
 		# Register shutdown hook
 		rospy.on_shutdown(self.on_shutdown)
 		 
 		 # Set up subscriber to twist_cmds coming from move_base package
 		self.twist_cmd_topic = rospy.get_param('~twist_cmd_topic', '/cmd_vel')
-		self.twist_cmd_sub =  rospy.Subscriber(self.twist_cmd_topic, Twist, self.cmd_callback, queue_size=10)
+		self.twist_cmd_sub =  rospy.Subscriber(self.twist_cmd_topic, Twist, self.twist_cmd_callback, queue_size=10)
 		
+		
+		# Set up subscriber to the wheel speeds topic for feedback to the PID throttle controller
+		self.wheelspeed_sub = rospy.Subscriber('/wheelSpeeds', wheelSpeeds, self.wheelSpeeds_callback, queue_size = 10)
 		
 		# Set up publisher for sending chassisCommands to the autorally core controller 
 		self.chassisCommand_topic = '/{}/chassisCommand'.format(self.name)
@@ -41,23 +65,66 @@ class BaseController():
 		
 		
 		# Grab parameters from the ros param server
-		self.wheelbase = rospy.get_param('/vehicle_wheelbase ', 1.0)
+		self.wheelbase = rospy.get_param('/vehicle_wheelbase ', 0.57)
 		self.frame_id = rospy.get_param('~frame_id', 'odom')
 		self.max_vel_x = rospy.get_param('/autorally_move_base/TebLocalPlannerROS/max_vel_x', 26.82 )
 		
 		
-		# Shared command values (set by twist_cmd callback, used for sending chassisCommands)
-		self.steering = -5
-		self.throttle = 0
-		self.frontBrake = -5
 		
-		self.load_throttle_calibration()
 		
 		if self.debugging:
 			rospy.loginfo('{} initialized!'.format(self.name))
 			
 		
-		self.run()
+		rospy.spin()
+		
+	
+	
+	def interpolatekey(self, goal_speed):
+		""" Accepts a goal speed (m/s) and returns throttle positioning required to achieve that speed.
+			Value is found by interpolating the throttle calibration file values """
+		top_mapped_speed = self.throttle_mappings.values()[-1]
+		
+		if goal_speed > top_mapped_speed:
+			rospy.logerr('Goal speed is higher than any defined in throttle calibration file! '\
+						 'Setting current speed to highest defined throttle: {} -> {} m/s'.format(self.throttle_mappings.keys()[-1], top_mapped_speed))
+			goal_speed = top_mapped_speed
+			
+		return np.interp(goal_speed, self.throttle_mappings.values(), self.throttle_mappings.keys())
+			
+			
+			
+	def wheelSpeeds_callback(self, data):
+		""" Listens to the autorally wheelSpeeds topic and issues new throttle commands based on the feed back from a PID controller """
+		
+		# Consider the average speed of the front two wheels 
+		self.front_wheel_speed = 0.5 * (data.lfSpeed + data.rfSpeed)
+		
+		
+		# This section of the code is adapted from ConstantSpeedController.cpp in the autorally_control package
+		if self.goal_speed > 0.01: # m/s
+			self.integral_error += self.goal_speed - self.front_wheel_speed
+			
+			if ( self.integral_error > (self.throttle_IMax / self.throttle_KI)):
+				self.integral_error = (self.throttle_IMax / self.throttle_KI)
+			
+			if ( self.integral_error < -(self.throttle_IMax / self.throttle_KI)):
+				self.integral_error = -(self.throttle_IMax / self.throttle_KI)
+			
+			throttle = self.interpolatekey(self.goal_speed) + self.throttle_KP * (self.goal_speed - self.front_wheel_speed)
+			throttle += self.throttle_KI + self.integral_error
+			throttle = max( 0.0 , min( 1.0 , throttle))
+			self.throttle = throttle
+			
+			
+		else:
+			self.throttle = 0.0
+			
+		self.steering = self.goal_steering
+		self.send_chassis_command_msg()
+		
+		
+		
 		
 		
 	def load_throttle_calibration(self):
@@ -77,12 +144,27 @@ class BaseController():
 			for line in f:
 				line = line.replace('\'',"")
 				(key, val) = line.split(':')
-				self.throttle_mappings[key] = float(val)
+				self.throttle_mappings[float(key)] = float(val)
 		
 		# Sort the dict
 		self.throttle_mappings = collections.OrderedDict(sorted(self.throttle_mappings.items()))
 		
 		
+#		print(self.throttle_mappings)
+#		print('\n\n')
+#		print(self.throttle_mappings[0.11])
+#		print('\n\n')
+#		print(self.throttle_mappings.keys())
+#		print('\n\n')
+#		print(self.throttle_mappings.values())
+#		print('\n\n')
+#		print(self.interpolatekey(25.1))
+		
+		
+	
+	
+	# Can delete
+	# This is for publishing messages at a different frequency than the subscribers messages come in at
 	def run(self):
 		while not rospy.is_shutdown():
 			r = rospy.Rate(10) # 10hz
@@ -102,29 +184,28 @@ class BaseController():
 		cmd.throttle = self.throttle
 		cmd.frontBrake = self.frontBrake
 		
+		print(cmd)
 		self.chassisCommand_pub.publish(cmd)
 	
 	
 	
-	def cmd_callback(self, data):
+	def twist_cmd_callback(self, data):
 		""" Callback for the cmd_vel twist message topic. """
-		# Might need to implement locks on the command values since the publishing and subscribing threads could touch them at the same time?
-		if self.debugging:
-			rospy.loginfo(data)
-
-		v = data.linear.x
-		steering = self.convert_trans_rot_vel_to_steering_angle(v, data.angular.z)
 		
-		
-		# Convert the msg velocity (m/s) to a normalized chassisCommand value
-#		#normalized_v = v / self.max_vel_x
-		normalized_v = v
-		
-		# Set the command values
-		self.throttle = normalized_v
-		self.steering = steering
+		if self.most_recent_twist_cmd != data:
+			if self.debugging:
+				rospy.loginfo('New twist command')
+				rospy.loginfo(data)
 			
-		
+			self.most_recent_twist_cmd = data
+
+			self.goal_speed = data.linear.x
+			self.goal_steering = self.convert_trans_rot_vel_to_steering_angle(v = data.linear.x, omega = data.angular.z)
+			self.goal_steering = ( self.goal_steering  / ( math.pi / 2))
+			
+			
+			
+
 	def convert_trans_rot_vel_to_steering_angle(self, v, omega):
 		""" Translates the angular z direction component from the twist message to a steering angle 
 			Code from http://wiki.ros.org/teb_local_planner/Tutorials/Planning%20for%20car-like%20robots
