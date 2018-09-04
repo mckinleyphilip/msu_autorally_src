df3938fc1421a83f97985fe11904314262e843b6
Glen Simon <simongle@msu.edu>
2018-09-04 19:57:15 -0400

    Added new fields for the LogEvent and EvoRs2State msgs to support the return of logging values after the completetion of the simulation. Currently working on getting the transportation of a 2D array to work within ROS messaging.

--- msu_autorally/evo_ros2/src/autorally_sim_manager.py
+++ msu_autorally/evo_ros2/src/autorally_sim_manager.py
@@ -39,6 +39,7 @@ class AutorallySimManagerNode():
 		# Set up threading event - Used to sync main thread and the evo-ros2 communication threads since the ros launch api has to be called from the main thread
 		self.event = threading.Event()
 		self.event.clear()
+		self.lock = threading.Lock()
 		
 		# Set up communication topics
 		self.set_up_evo_ros2_communications()
@@ -46,6 +47,7 @@ class AutorallySimManagerNode():
 		# Set up other member variables
 		self.log_event_times = []
 		self.sim_start_time = 0
+		self.result_headers = []
 		
 		while not rospy.is_shutdown():
 			
@@ -75,16 +77,44 @@ class AutorallySimManagerNode():
 					self.log_event_trigger()
 					self.sleep_rate.sleep()
 				
-				self.end_sim()
-				self.set_evo_ros2_state(6)
+				result_msg = self.end_sim()
+				self.set_evo_ros2_state(6, msg = result_msg)
 				continue
 		
 
 	def log_collection_cb(self, msg):
-		self.collected_logs.append(msg.log_data)
+		if self.debug:
+			rospy.logwarn('Received log data from: {}'.format(msg.sender))
 		
-		print('Received log data!')
-
+		self.lock.acquire()
+		
+		try:
+			if self.debug:
+				rospy.logwarn('Processing log data from: {}'.format(msg.sender))
+		
+			self.collected_logs.append(msg.log_data)
+			self.result_headers.append(msg.data_header)
+		finally:
+			self.lock.release()
+		
+		
+	def compile_results(self):
+		
+		self.results = []
+		
+		#print('\n\nheaders')
+		#print(self.result_headers)
+		
+		#print('\n\nCollected Logs')
+		#print(self.collected_logs)
+		
+		for index, header in enumerate(self.result_headers):
+			self.results.append((header, self.collected_logs[index]))
+		
+		if self.debug:
+			rospy.loginfo('\n\nResults')
+			rospy.loginfo(self.results)
+		
 		
 	def log_event_trigger(self):
 		current_time = 0
@@ -101,8 +131,10 @@ class AutorallySimManagerNode():
 		
 		self.log_event_times.append(sim_time.to_sec())
 
+
 	def log_event_request(self):
 		self.collected_logs = []
+		self.result_headers.append('time')
 		self.collected_logs.append(self.log_event_times)
 		
 		current_time = 0
@@ -133,9 +165,10 @@ class AutorallySimManagerNode():
 			sleep_rate.sleep()
 		
 		
-		
 		# Get list of ros nodes that are present before the mission launch file is started
 		pre_mission_nodes = rosnode.get_node_names()
+		self.utility_monitors_nodes = list(set(pre_mission_nodes) - set(pre_utility_monitors_nodes))
+		
 		
 		# Start mission launch file which is responsible for controlling the platform through the simulation
 		self.mission_launch = roslaunch.parent.ROSLaunchParent(self.uuid, roslaunch.rlutil.resolve_launch_arguments(self.mission_launch_info ))
@@ -159,15 +192,27 @@ class AutorallySimManagerNode():
 	def end_sim(self):
 		self.log_event_request()
 		
-		raw_input('press enter to advance')
+		if self.debug:
+			rospy.logwarn('Utility Monitor Nodes: {}'.format(self.utility_monitors_nodes))
 		
-		print(self.collected_logs)
 		
-		
-		raw_input('press enter to advance')
+		while len(self.collected_logs) <= len(self.utility_monitors_nodes):
+			print('Waiting on results...')
+			self.sleep_rate.sleep()
+					
+					
+		self.compile_results()		
 		
 		self.mission_launch.shutdown()
 		self.utility_monitors_launch.shutdown()
+		
+		msg = EvoROS2State()
+		msg.result_headers = self.result_headers
+		
+		for log_array in self.collected_logs:
+			msg.result.append(log_array)
+		#msg.result = self.collected_logs
+		return msg
 		
 	
 	def set_up_evo_ros2_communications(self):
@@ -181,9 +226,8 @@ class AutorallySimManagerNode():
 		self.log_collection_topic = rospy.Subscriber(rospy.get_param('EVO_ROS_LOG_COLLECTION_TOPIC'), LogEvent, self.log_collection_cb)
 	
 	
-	def set_evo_ros2_state(self, new_state_value):
+	def set_evo_ros2_state(self, new_state_value, msg = EvoROS2State()):
 		rospy.set_param('evo_ros2_state', new_state_value)
-		msg = EvoROS2State()
 		msg.sender = self.node_name
 		msg.state = new_state_value
 		self.evo_ros2_comm_pub.publish(msg)
