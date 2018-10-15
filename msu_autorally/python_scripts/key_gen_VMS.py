2dbc38301976b1c03cc8c6366c3daab0e7fcc096
Glen Simon <glen.a.simon@gmail.com>
2018-10-15 13:05:52 -0400

    Editted robo distrobution scripts

--- msu_autorally/python_scripts/key_gen_VMS.py
+++ msu_autorally/python_scripts/key_gen_VMS.py
@@ -4,20 +4,17 @@ import os
 import yaml
 import re
 
-parser = argparse.ArgumentParser(description='Script used for starting transport_controller.py on all of the robo VMs')
+parser = argparse.ArgumentParser(description='Script used for updating the ros_catkin_ws code on all of the robo VMs from the github repo')
 #parser.add_argument('-r', '--remote', action='store_true', help='Use when not on MSU Engineering network. SSH\'s into arctic server before going to robo servers')
 #parser.add_argument('-d', '--debug', action='store_true', help='Print extra output to terminal, spawn subprocesses in xterm for seperated process outputs')
 #parser.add_argument('-p', '--password', type=str, help='Users password on remote machine. This is a required parameter')
 args= parser.parse_args()
 
-print('Starting software_manager.py scripts on robo nodes...')
+print('Starting update scripts on robo nodes...')
 
-name_of_script = "software_manager.py"
-GA_IP_ADDR = '35.9.28.201'
-script_arguments = "-ip {}".format(GA_IP_ADDR)
-ga_hostname = 'autorally-robo1vm1'
 
 work_nodes_file_name = 'active_nodes.yml'
+
 
 with open(os.path.dirname(os.path.abspath(__file__)) + '/{}'.format(work_nodes_file_name), 'r') as ymlfile:
 	cfg = yaml.load(ymlfile)
@@ -27,18 +24,11 @@ for worker in cfg['worker_list']:
 	ip = cfg['worker_list'][str(worker)]['ip']
 	print(str(ip))
 	
-	if str(worker) is ga_hostname:
-		continue
-
-	#Start Software Manager
-	cmds = """echo {}...;
-		cd;
-		roslaunch evo_ros2 {} {};
-		exec bash
-		""".format(name_of_script, name_of_script, script_arguments)
-	cmd_str = """xterm -hold -title "Connection to {}" -e 'ssh -t -X {}.cse.msu.edu "{}"'&""".format(worker,worker,cmds)
+	cmds = """
+		ssh-copy-id simongle@'{}';
+		""".format(ip)
+	cmd_str = 'xterm -title "Connection to {}" -hold -e "{}"&'.format(worker,cmds)
 	os.system(cmd_str)
-
 
 print('Script finished! \n')
 
@@ -46,3 +36,4 @@ print('Press enter to close all xterm windows and close this script...')
 _ = raw_input()
 cmd_str = "pkill xterm"
 os.system(cmd_str)
+
