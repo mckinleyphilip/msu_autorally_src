8eb6e62f5fc4db83ceb5ce8b89816e51d349d383
Glen Simon <glen.a.simon@gmail.com>
2018-11-01 14:02:49 -0400

    Added script for installing hector sensors across the robos

---
+++ msu_autorally/python_scripts/install_hector_plugins_VMs.py
@@ -0,0 +1,45 @@
+import argparse
+import subprocess
+import os
+import yaml
+import re
+
+parser = argparse.ArgumentParser(description='Script used for updating the ros_catkin_ws code on all of the robo VMs from the github repo')
+#parser.add_argument('-r', '--remote', action='store_true', help='Use when not on MSU Engineering network. SSH\'s into arctic server before going to robo servers')
+#parser.add_argument('-d', '--debug', action='store_true', help='Print extra output to terminal, spawn subprocesses in xterm for seperated process outputs')
+#parser.add_argument('-p', '--password', type=str, help='Users password on remote machine. This is a required parameter')
+args= parser.parse_args()
+
+print('Starting update scripts on robo nodes...')
+
+
+work_nodes_file_name = 'active_nodes.yml'
+#work_nodes_file_name = 'test_nodes.yml'
+git_branch = "PID_evol_experiment"
+
+with open(os.path.dirname(os.path.abspath(__file__)) + '/{}'.format(work_nodes_file_name), 'r') as ymlfile:
+	cfg = yaml.load(ymlfile)
+
+for worker in cfg['worker_list']:
+	print(str(worker))
+	ip = cfg['worker_list'][str(worker)]['ip']
+	print(str(ip))
+	
+	cmds = """echo 'Forcing all ros_catkin_ws/src code to match Github';
+		source /opt/ros/indigo/setup.bash;
+		source ~/autorally_catkin_ws/devel/setup.sh;
+		source ~/autorally_catkin_ws/src/autorally/autorally_util/setupEnvLocal.sh;
+		cd autorally_catkin_ws/;
+		catkin_make --pkg hector_gazebo_plugins;
+		exec bash
+		""".format(git_branch)
+	cmd_str = 'xterm -title "Connection to {}" -hold -e ssh -t -X {} "{}"&'.format(worker,ip,cmds)
+	os.system(cmd_str)
+
+print('Script finished! \n')
+
+print('Press enter to close all xterm windows and close this script...')
+_ = raw_input()
+cmd_str = "pkill xterm"
+os.system(cmd_str)
+
