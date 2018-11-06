cc82d5d99b4a416297e52cb6696fe1d730c84063
Glen Simon <simongle@msu.edu>
2018-11-06 08:16:07 -0500

    Generated plots for Incline PID Tuning runs

---
+++ msu_autorally/evo_ros2/src/EAs/logs/PID-Tuning-No-Braking-Function/matlab_test_script.m
@@ -0,0 +1,30 @@
+%% Import best individual from each run
+%
+%
+% GAS 11-5-17
+
+%clear all;
+number_of_runs = 10;
+
+headers = {'Run_Number', 'Best_Fitness', 'IMax', 'KD', 'KI', 'KP'};
+comp_table_2 = cell2table(cell(0,6));
+comp_table_2.Properties.VariableNames = headers;
+
+run_directories = dir();
+
+for i=1:length(run_directories)
+	if run_directories(i).isdir
+		dir_name = run_directories(i).name;
+		if contains(dir_name, 'run')
+			filename = strcat(dir_name, '/','log.json');
+			table = jsondecode(fileread(filename));
+			IMax = table.best_ind(1);
+			KD = table.best_ind(2);
+			KI = table.best_ind(3);
+			KP = table.best_ind(4);
+			comp_table_2 = [comp_table_2; {table.run_number, table.best_ind_fitness, IMax, KD, KI, KP}];
+		end
+	end
+end
+
+
