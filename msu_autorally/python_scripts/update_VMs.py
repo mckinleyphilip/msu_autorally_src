f7640ef156e1587d8ae054f6af52d148978c4a63
Glen Simon <simongle@msu.edu>
2018-11-08 14:58:54 -0500

    .

--- msu_autorally/python_scripts/update_VMs.py
+++ msu_autorally/python_scripts/update_VMs.py
@@ -13,8 +13,8 @@ args= parser.parse_args()
 print('Starting update scripts on robo nodes...')
 
 
-work_nodes_file_name = 'all_nodes.yml'
-#work_nodes_file_name = 'test_nodes.yml'
+#work_nodes_file_name = 'all_nodes.yml'
+work_nodes_file_name = 'update_nodes.yml'
 git_branch = "PID_evol_experiment"
 
 with open(os.path.dirname(os.path.abspath(__file__)) + '/{}'.format(work_nodes_file_name), 'r') as ymlfile:
