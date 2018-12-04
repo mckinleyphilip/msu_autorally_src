082c88658b2b6763be206ef4acb33dc510b60510
Jared Clark <jared@robo10vm2u16.cse.msu.edu>
2018-12-04 12:59:53 -0500

    ran a slightly longer run

--- msu_autorally/evo_ros2/src/EAs/DEAP_EA_Speed_PID_Tuning_Jared.py
+++ msu_autorally/evo_ros2/src/EAs/DEAP_EA_Speed_PID_Tuning_Jared.py
@@ -50,8 +50,8 @@ class DEAP_EA():
 		self.experiment_name = "PID-Incline-Tuning-NoBrakes-HighRes"
 		self.genome_size = 4
 		self.tourn_size = 2
-		self.pop_size = 9
-		self.number_generations = 10
+		self.pop_size = 15
+		self.number_generations = 20
 
 		starting_run_number = 1
 		number_of_runs = 1
